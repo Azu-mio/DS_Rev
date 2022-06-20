4c54821d027c1ae0e9c5073ec8b95fbe52adbb81
misakaovo@163.com <misakaovo@163.com>
2022-06-21 01:23:44 +0800

    完成了顺序表插入操作

--- 顺序表/SqList.cpp
+++ 顺序表/SqList.cpp
@@ -3,8 +3,9 @@
 
 typedef struct{
     // int data[MaxSize];
-    int data[MaxSize]={1,2,3,4,5,6};
-    int length;
+    int data[MaxSize]={1,2,3,4,5,6,7,8,9,10};
+    // int length;
+    int length = 10;
 }SqList;
 
 
@@ -16,9 +17,32 @@ void InitList(SqList &L)
    L.length=0;
 }
 
+void ListInsert(SqList &L,int i,int e){
+    for(int j=L.length;j>=i;j--){
+       L.data[j]=L.data[j-1]; 
+    }
+    L.data[i-1]=e;
+    L.length++;
+}
+
 int main(){
     SqList L;
-    // int a[10] = {1,2,3};
+    // InitList(L);
+    for(int i=0;i<L.length;i++){printf("%d ",L.data[i]);} printf("\n");
+    ListInsert(L,3,100);
+    for(int i=0;i<L.length;i++){printf("%d ",L.data[i]);} printf("\n");
+ 
+}
+
+
+
+// {
+//     常用代码
+//      for(int i=0;i<L.length;i++){printf("%d ",L.data[i]);} printf("\n");
+// }
+// {
+       
+       // int a[10] = {1,2,3};
     // for(int i=0;i<10;i++){
     //     printf("%d",a[i]);
     // }
@@ -26,18 +50,18 @@ int main(){
     
     // L.data[0,1,2]={1,2,3};
   
-    printf("L");
-    for(int i=0;i<10;i++){
-        printf("%d ",L.data[i]);
-    }
-    InitList(L);
+//     printf("L");
+//     for(int i=0;i<10;i++){
+//         printf("%d ",L.data[i]);
+//     }
+//     InitList(L);
 
-    printf("L0length%d ",L.length);
+//     printf("L0length%d ",L.length);
 
-    printf("L");
-    for(int i=0;i<10;i++){
-        printf("%d ",L.data[i]);
-    }
-    printf("Llength%d ",L.length);
-    return 0;
-}
+//     printf("L");
+//     for(int i=0;i<10;i++){
+//         printf("%d ",L.data[i]);
+//     }
+//     printf("Llength%d ",L.length);
+//     return 0;
+//  }
